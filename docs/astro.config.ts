4140fd2731d07264b2aaaeefebf615c49e79510b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-10-26 20:40:30 +0100

    docs: add og image

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,6 +2,11 @@ import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
 import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian'
 
+const site =
+  process.env['VERCEL_ENV'] !== 'production' && process.env['VERCEL_URL']
+    ? `https://${process.env['VERCEL_URL']}`
+    : 'https://starlight-obsidian.vercel.app/'
+
 export default defineConfig({
   integrations: [
     starlight({
@@ -9,6 +14,13 @@ export default defineConfig({
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/starlight-obsidian/edit/main/docs/',
       },
+      head: [
+        {
+          tag: 'meta',
+          attrs: { property: 'og:image', content: new URL('og.jpg', site).href },
+        },
+        { tag: 'meta', attrs: { property: 'og:image:alt', content: 'Starlight plugin to publish Obsidian vaults.' } },
+      ],
       plugins: [
         starlightObsidian({
           output: 'demo',
@@ -48,4 +60,5 @@ export default defineConfig({
       title: 'Starlight Obsidian',
     }),
   ],
+  site,
 })
